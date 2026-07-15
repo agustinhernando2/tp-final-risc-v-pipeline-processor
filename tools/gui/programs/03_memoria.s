18b9f304fb9e6efeed674e9df34b17eb917036ed
Agustin Hernando <agustin.hernando@craftech.io>
2026-07-15 13:38:30 -0300

    add

--- tools/gui/programs/03_memoria.s
+++ tools/gui/programs/03_memoria.s
@@ -8,11 +8,12 @@
 #
 # Resultado esperado:
 #   mem[0] = 0x000000FF
-#   x10 (lb  mem[0]) = -1   (0xFF con signo: bits altos en 1)
+#   x10 (lb  mem[0]) = -1   = 0xffffffff (sign-extend)
 #   x11 (lbu mem[0]) = 255  (0x000000FF, sin signo)
-#   mem[1] = 0x00008000
-#   x12 (lh  mem[1]) negativo (bit 15 en 1, sign-extend)
-#   x13 (lhu mem[1]) = 0x00008000 (zero-extend)
+#   mem[1] = 0x10008003
+#   x12 (lh  mem[1]) = 0xffff8003 (sign-extend)
+#   x13 (lhu mem[1]) = 0x00008003 (zero-extend)
+#   x14 (lw mem[1]) = 0x10008003 (zero-extend)
 
         addi x1, x0, 255      # 0x000000FF
         sw   x1, 0(x0)        # mem[0] = 0x000000FF
@@ -20,12 +21,11 @@
         lb   x10, 0(x0)       # byte CON signo  -> 0xFF se interpreta como -1
         lbu  x11, 0(x0)       # byte SIN signo  -> 255
 
-        lui  x2, 0x8          # x2 = 0x00008000 (bit 15 en 1)
-        sw   x2, 1(x0)        # mem[1] = 0x00008000
-        lh   x12, 1(x0)       # halfword CON signo -> negativo
-        lhu  x13, 1(x0)       # halfword SIN signo -> 0x00008000
-        lw   x14, 1(x0)       # word completo      -> 0x00008000
+        lui  x2, 0x10008      # x2 = 0x10008000 lui carga la parte alta, los primeros 12 bits se carcan con addi
+        addi x2, x2, 0x3      # x2 = 0x10008003
+        sw   x2, 1(x0)
+        lh   x12, 1(x0)
+        lhu  x13, 1(x0)
+        lw   x14, 1(x0)
 
-        sh   x1, 2(x0)        # store halfword -> mem[2] = 0x00FF
-        sb   x1, 3(x0)        # store byte     -> mem[3] = 0xFF
         halt
